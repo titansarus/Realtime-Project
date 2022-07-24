c75b0c47ed01c5544a25d62f5f3be8cc2953560c
Amirmahdi Namjoo <amirm137878@gmail.com>
2022-07-24 05:05:39 +0430

    Create ThreadBlock.java

---
+++ clock/ThreadBlock.java
@@ -0,0 +1,33 @@
+import javolution.util.ReentrantLock;
+
+
+
+public class ThreadBlock implements Comparable<ThreadBlock> {
+    private final ClockThread clockThread;
+    private final UIThread uiThread;
+    private final ReentrantLock lock;
+
+    public ThreadBlock(ClockThread clockThread, UIThread uiThread) {
+        this.clockThread = clockThread;
+        this.uiThread = uiThread;
+        this.lock = new ReentrantLock();
+        this.getClockThread().setLock(lock);
+    }
+
+    @Override
+    public int compareTo(ThreadBlock o) {
+        return this.clockThread.getPriority() - o.clockThread.getPriority();
+    }
+
+    public ClockThread getClockThread() {
+        return clockThread;
+    }
+
+    public UIThread getUiThread() {
+        return uiThread;
+    }
+
+    public ReentrantLock getLock() {
+        return lock;
+    }
+}
